0f3e7eeebb57fec5188978213656b24e42ffccbb
Travis McDemus <travis@perka.com>
2016-11-04 18:46:08 -0400

    Add superfluous weather data

--- app/src/main/java/aoeu/foretell/MainActivity.java
+++ app/src/main/java/aoeu/foretell/MainActivity.java
@@ -7,7 +7,7 @@ import android.webkit.WebView;
 
 public class MainActivity extends Activity {
 
-  final Uri weatherUri = Uri.parse("http://forecast.weather.gov/meteograms/Plotter.php?lat=40.6447&lon=-73.9472&wfo=OKX&zcode=NYZ075&gset=20&gdiff=10&unit=0&tinfo=EY5&ahour=0&pcmd=11010111111110100000000000000000000000000000000000000000000&lg=en&indu=1!1!1!&dd=&bw=&hrspan=24&pqpfhr=6&psnwhr=6");
+  final Uri weatherUri = Uri.parse("http://forecast.weather.gov/meteograms/Plotter.php?lat=40.704&lon=-73.946&wfo=OKX&zcode=NYZ075&gset=20&gdiff=10&unit=0&tinfo=EY5&ahour=0&pcmd=11011111111110111000000000000000000000000000000000000000000&lg=en&indu=1!1!1!&dd=&bw=&hrspan=24&pqpfhr=6&psnwhr=6");
   WebView view;
 
   @Override
